864fc779d0b0af87ee7b5c8cf7bd8a50f3312cdd
argo <argo@MAQUETA>
2023-12-05 13:09:45 +0100

    Add project files.

---
+++ Actividades/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Actividades
+{
+    public class Ex16
+    {
+        public static void Main()
+        {
+            int num = 0;
+
+            Console.WriteLine(num);
+            Console.WriteLine();
+        }
+    }
+}
